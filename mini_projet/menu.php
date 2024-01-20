ae8eebeb2be7c65393365fccc54e32d1969c6cc7
Judolphin <139502720+Judolphin@users.noreply.github.com>
2024-01-20 16:19:05 +0100

    Add files via upload

---
+++ mini_projet/menu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+  
+<head>
+  <meta charset="utf-8">
+  <title>Assistances technologiques pour les malvoyants</title>
+ 
+</head>
+<body>
+  <div id="accessibilite">
+    <header>
+      <nav>
+        <a href="accueil.html">Accueil</a>
+        <a href="aides_techniques.html">Aides Techniques</a>
+        <a href="defis.html">Défis</a>
+        <a href="perspectives.html">Perspectives</a>
+        <a href="sitographie.html">Sitographie</a>
+        <div id="accessibility-menu">
+          <button class="accessibility-button" onclick="toggleAccessibilityMenu()">Accessibilité</button>
+          <div class="menu-options"></div>
+        </div>
+      </nav>
+      <img src="multi_media\logo.jpg" alt="Logo: Œil incomplet, site équipé pour les malvoyants">
+      <h1>Assistances technologiques pour les malvoyants</h1>
+      <p>Une aide visuelle par la technologie</p>
+    </header>
+
+    <?php
+      /*  code ici */
+    ?>
+  </table>
+  </body>
+</html>
